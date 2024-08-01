852bc12a2844c8df0c5e6ba40989db8d1696659f
Alexis Guzman <spartanthelexvro@gmail.com>
2024-07-31 19:06:14 -0600

    Tres algoritmos completos

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-//Datos de prueba
-const data = [
-  -15, 72, 56, 82, 12, 7, -85, -47, 40, 25, 9, -59, -86, -88, 34, 86, 43, -38,
-  -54, -27, 29, 3, -19, -69, 57, 47, -9, 85, 63, 11, -29, 74, -68, 67, 16, 20,
-  -21, 4, 52, 30, -56, 69, 14, 48, -76, 59, -42, -93, -13, 5, -5, 35, 27, -99,
-  -24, 77, -89, 71, 56, 36, -32, -94, -34, 19, 17, -45, 22, -6, 99, 24, 90, 39,
-  61, 38, 68, 91, 92, -61, -11, -28, 23, 81, -63, 33, 55, 70, 64, 42, 78, 50,
-  -3, -30, -80, -12, 79, -31, 21, 60, 49, -55, 54, 2, 8, 6, 65, 32, -10, -46,
-  58, -41, 26, 45, -95, -4, -96, 87, 95, 51, 10, -44, -98, 76, 46, 18, 83, 44,
-  98, -16, 89, 72, 66, 37, 62, 53, -91, 28, -2, 41, -14, 84, 1, -90, 88, -77,
-  73, 31, 13, 15, 80, 75, 100, -8, 93, -40, -97, 97, 94, 96, -57, -60, -7, -18,
-  -70, -17, -1, -48, -36, -50, -83, -84, -35, -25, -43, -52, -87, -33, -49, -22,
-  -58, -64, -20, -37, -66, -92, -67, -51, -65, -26, -74, -71, -23, -78, -62,
-  -53, -75, -39, -60, -100, -79, -72, -81, -73,
-];
-const number = -30;
+// Función para generar un array de números enteros de -500 a 500
+const generarArray = () => {
+    const start = 0;
+    const end = 80000;
+    let array = [];
+
+    for (let i = start; i <= end; i++) {
+        array.push(i);
+    }
+
+    return array;
+};
+
+// Función para mezclar el array de forma aleatoria
+const mezclarArray = arr => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // Intercambiar elementos
+    }
+    return arr;
+};
+
+// Generar y mezclar el array
+const data = mezclarArray(generarArray());
+console.log(data)
+
+const number = 9000000;
 
 //TODO para la construccion del algoritmo O(n^2)
 const searchIndexsONE = (num) => {
@@ -34,7 +45,45 @@ const searchIndexsONE = (num) => {
   return dataIndex;
 };
 
-//TODO para construir algoritmo O(log n)
+//TODO para construir algoritmo O(n log n)
+const sortArray = (arr) => {
+    return arr.sort((a, b) => a - b);
+}
+
+const binarySearch = (arr, complement) => {
+    let letf = 0;
+    let rigth = arr.length - 1;
+    while (letf <= rigth) {
+        let mid = Math.floor((letf + rigth) / 2);
+        if (arr[mid] === complement) {
+            return true;
+        } else if (arr[mid] < complement) {
+            letf = mid + 1;
+        } else {
+            rigth = mid - 1;
+        }
+    }
+    return false;
+}
+
+const searchIndexsTRHEE = (num) => {
+    let dataIndex = [];
+    let arr = sortArray(data);
+
+    // Recorrer cada elemento del array
+    for (let i = 0; i < arr.length; i++) {
+        let complement = num - arr[i];
+
+        // Buscar el complemento usando búsqueda binaria
+        if (binarySearch(arr, complement)) {
+            dataIndex.push(arr[i]);
+            dataIndex.push(complement);
+        }
+    }
+    return dataIndex;
+}
+
+
 
 //TODO para la construccion del algoritmo O(n)
 const insertToMap = () => {  //Se construye un diccionario
